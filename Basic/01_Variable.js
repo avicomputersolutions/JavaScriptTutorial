13f554ebb183ec7f423b0dda2ed12c5505bed4be
avicomputersolutions <30495311+avicomputersolutions@users.noreply.github.com>
2024-12-08 03:26:16 +0000

    Variables added

---
+++ Basic/01_Variable.js
@@ -0,0 +1,9 @@
+const moneyRequired=10000
+let balance=200
+var Name="Avi"
+accountNo=12345
+
+/* var has scope issue so use let instead of var */
+//moneyRequired=2000  // not allowed
+console.log(moneyRequired);
+console.table([balance,Name,accountNo])
